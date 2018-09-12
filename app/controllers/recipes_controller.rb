564990366d6929f01380e24db73013c41a88e060
github username <github email address>
2018-09-12 15:30:29 -0400

    form partial for creating a recipe, title and description, and then print out to the index view persisting to the database

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @recipes = Recipe.order("created_at DESC")
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image)
+  end
 end
