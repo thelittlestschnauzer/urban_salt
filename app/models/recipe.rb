8ab8116ed3264eeda2f3c5fea616e6ab88b9c8d3
github username <github email address>
2018-09-12 21:36:43 -0400

    added gems minimagick and file-validators

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,7 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { maximum: 140 }
 
+  validates :image, file_size: { less_than: 2.megabytes }
+
+  mount_uploader :image, ImageUploader
 end
